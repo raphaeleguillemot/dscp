89801168246780e58a5972bff1de1e81d420fa86
raphaeleguillemot <ps20211223@student.vsa.edu.hk>
2020-10-15 13:40:38 +0800

    responsive, now hmenu hmm

---
+++ main.js
@@ -0,0 +1,11 @@
+var icon = document.getElementById("menu-toggle");
+var dropdown = document.getElementsByTagName("ul");
+  
+icon.onclick = function() {
+  if(dropdown.style.display == "none"){
+    dropdown.style.display = "block";
+  }
+  else{
+    dropdown.style.display = "none";
+  }
+}  
