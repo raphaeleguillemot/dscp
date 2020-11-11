dd605f211f2036ac54d4a7fc9a51c9bb40c15536
raphaeleguillemot <ps20211223@student.vsa.edu.hk>
2020-11-11 11:45:15 +0800

    ugly dropdown FINALLY WORKS

--- main.js
+++ main.js
@@ -1,20 +1,14 @@
  $(document).ready(function(){
-	var pid = ($("#menu-dropdown-list").length && $("#menu-dropdown-list").attr('class')) ? $("#menu-dropdown-list").attr('class'): "";
-
 	$("#menu-toggle").click(function(){
-		console.log($("#menu-dropdown-list").css("display"));
-		if($("#menu-dropdown-list").css("display", "none")){
-			console.log("HEY");
+		if($("#menu-dropdown-list").css("display") == "none"){
 			$("#menu-dropdown-list").css("display", "block");
 		}
-		else{
-			console.log("THIS SHOULD LOAD");
+		else if($("#menu-dropdown-list").css("display")=="block"){
 			$("#menu-dropdown-list").css("display", "none");
 		}
 	});
 	
 	$("#menu-dropdown-list").click(function(){
-		console.log("PLEASE");
 		$("#menu-dropdown-list").css("display", "none");
 	});
   });
