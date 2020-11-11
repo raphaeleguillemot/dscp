10bd1a00cb0e48e339483b0d75d3a028c26a20c1
raphaeleguillemot <ps20211223@student.vsa.edu.hk>
2020-11-11 10:43:10 +0800

    argh typo fix

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
  $(document).ready(function(){
- 	
- 	var pid = ($("3menu-dropdown-list").length && $("#menu-dropdown-list").attr('class')) 
-    ? $("#menu-dropdown-list").attr('class') 
-    : "";
+ var pid = ($("#menu-dropdown-list").length && $("#menu-dropdown-list").attr('class')) ? $("#menu-dropdown-list").attr('class'): "";
 
 	$("#menu-toggle").click(function(){
-		if(pid.style.display == "none"){
-			pid.style.display = "block";
+		if($(pid).style.display == "none"){
+			$(pid).style.display = "block";
 		}
 		else{
-			pid.style.display = "none";
+			$(pid).style.display = "none";
 		}
 	});
 	
-	pid.click(function(){
-		$(this).style.display = "none";
+	$(pid).click(function(){
+		$(pid).style.display = "none";
 	});
   });
